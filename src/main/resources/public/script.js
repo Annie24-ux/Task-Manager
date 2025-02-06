a7bc5bd14b627974dbad54c87bf671a8cd7e0561
Siphesihle <sibiyasa24@gmail.com>
2025-02-07 01:22:06 +0200

    Added functionality.

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,2 +1,9 @@
 const newTaskButton = document.selectElementById("newTask");
 const submitButton = document.selectElementById("submit");
+
+window.addEventListener("load", fetchTasks);
+
+
+function fetchTasks(){
+    
+}
